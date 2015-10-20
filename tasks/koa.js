4820386a48220e7bde17db0f82a218ad1df0770c
Jeff Valore <rally25rs@yahoo.com>
2015-10-19 20:48:58 -0400

    install koa static and middleware

--- tasks/koa.js
+++ tasks/koa.js
@@ -1,13 +1,25 @@
 'use strict';
 
 var koa = require('koa');
+var koaStatic = require('koa-static');
+var path = require('path');
+
+var SOCKET_TIMEOUT = 2000;
+var DEFAULT_PORT = 8000;
 
 module.exports = function(grunt) {
    	grunt.registerMultiTask('koa', 'Start a koa web server.', function() {
 	    var done = this.async();
 	    var httpServer;
 	    var koaApplication = koa();
-	    var port = 8080;
+	    
+        // Merge task-specific options with these defaults.
+		var options = this.options({
+			port: DEFAULT_PORT,
+			static: null,
+			livereload: false,
+			middleware: null
+		});
 
 		var addKoaErrorHandling = function() {
 			koaApplication.on('error', function(err, errorContext){
@@ -21,6 +33,33 @@ module.exports = function(grunt) {
 			});
 		};
 
+		var addKoaMiddleware = function() {
+			var staticPath;
+
+			if(options.static) {
+				staticPath = path.join(process.cwd(), options.static);
+				grunt.log.writeln('  Serving static files from: ' + staticPath);
+				koaApplication.use(koaStatic(staticPath, {
+				}));
+			}
+
+			if(options.middleware) {
+				if(typeof(options.middleware) === 'function') {
+					options.middleware(koaApplication);
+				} else if (options.middleware instanceof Array) {
+					for(var i = 0; i < options.middleware.length; i++) {
+						koaApplication.use(options.middleware[i]);
+					}
+				} else {
+					grunt.log.error('koa.options.middleware must be a function or an array of middlewares.');
+					done();
+				}
+			}
+		};
+
+		// we attempt a graceful shutdown by calling server.close() which will stop listening for requests
+		// the server will then halt and this callback will be called when all sockets have timed out.
+		// this is why we set the socket timeout to shorter than the default, otherwise it would take 2 min to close the server.
 		var closeKoaServer = function() {
 		    grunt.log.write('Stopping Koa Server...');
 
@@ -37,11 +76,14 @@ module.exports = function(grunt) {
 	    grunt.log.writeln('Starting Koa Server...');
 
 	    addKoaErrorHandling();
-	    process.on('SIGINT', onSigint);
+	    addKoaMiddleware();
 
-	    httpServer = koaApplication.listen(port, function () {
-	    	grunt.log.writeln('  Koa server listening on http://localhost:' + port);
-	    	grunt.log.writeln('  Press <Ctrl>-c to stop the Koa server.');
+	    process.on('SIGINT', onSigint); // triggered when the user presses ctrl+c
+
+	    httpServer = koaApplication.listen(options.port, function () {
+	    	grunt.log.writeln('  Koa server listening on http://localhost:' + options.port);
+	    	grunt.log.writeln('  Press <ctrl>+c to stop the Koa server.');
 	    });
+	    httpServer.setTimeout(SOCKET_TIMEOUT);
 	});
 };
